be2e15ede1ac64f43275116d27fc58bcc03f58a1
gittestingcode <66916353+gittestingcode@users.noreply.github.com>
2020-12-08 21:17:48 -0500

    4

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
-import logo from './logo.svg';
+import exchange from './AAExchangelogo.jpg';
 import collage from './collageBIG2.jpg';
+import bottom from './Bottombar.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Test line
-        </p>
-        <a
-          className="Mantels-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Test link
-        </a>
-      </header>
-      <body className="App-body">
-      <img src={collage} className="App-collage" alt="collage" />
-      </body>
+      <div className="App-wall">
+        <header className="App-header">
+          <div className="App-exchange">
+          <img src={exchange} className="App-logo" alt="exchange" />
+          </div>
+          <div className="Street">
+          <p>
+            721 North Second Street</p>
+          <p>
+            Philadelphia, PA 19123</p>
+            </div>
+          <a
+            className="Mantels-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Test link
+          </a>
+        </header>
+        <body className="App-body">
+          <div className="App-bodypage">
+            <img src={collage} className="App-collage" alt="collage" />
+            <div className="App-bottom">
+              <img src={bottom} className="App-bottom" alt="bottom" />
+            </div>
+          </div>
+        </body>
+      </div>
     </div>
   );
 }
